cc4f18cf6739aa25d14603e48770dfbd6791dc69
ranjansaga <ranjanprabhub@gmail.com>
2025-05-16 21:59:46 +0530

    Update infiniteCurryingSumOfNumbers.js

--- infiniteCurryingSumOfNumbers.js
+++ infiniteCurryingSumOfNumbers.js
@@ -11,3 +11,19 @@ function sum(a) {
 }
 
 console.log(sum(1)(2)(3)()); // ✅ 6
+
+// currying with variable arguments
+function sum(...args) {
+  let total = args.reduce((acc, ele) => acc + ele, 0)
+  return function inner(...args) {
+    console.log("args", args)
+    if (args.length === 0) {
+      return total
+    } else {
+      total = total + args.reduce((acc, ele) => acc + ele, 0)
+    }
+    return inner
+  }
+}
+
+console.log(sum(1, 2)(3)(2)())
